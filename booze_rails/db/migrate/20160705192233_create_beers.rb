42fdd6f454c8a0df0ea29303784030f837ade458
Andrew Young <prince.of.utah@gmail.com>
2016-07-05 15:27:49 -0400

    created and migrated the models and migrations

---
+++ booze_rails/db/migrate/20160705192233_create_beers.rb
@@ -0,0 +1,12 @@
+class CreateBeers < ActiveRecord::Migration
+  def change
+    create_table :beers do |t|
+      t.string :name
+      t.string :brewery
+      t.string :style
+      t.integer :abv
+
+      t.timestamps null: false
+    end
+  end
+end
--- booze_rails/db/schema.rb
+++ booze_rails/db/schema.rb
@@ -11,6 +11,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160705192556) do
+
+  create_table "beers", force: :cascade do |t|
+    t.string   "name"
+    t.string   "brewery"
+    t.string   "style"
+    t.integer  "abv"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "breweries", force: :cascade do |t|
+    t.string   "name"
+    t.string   "neighborhood"
+    t.string   "beer"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string   "name"
+    t.string   "neighborhood"
+    t.string   "beer"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
 end
